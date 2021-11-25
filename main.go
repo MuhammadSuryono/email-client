349deec3a325c622ec3cf6dc69b3daa7e50310e1
Muhammad Suryono <msuryono0@gmail.com>
2021-11-25 08:56:54 +0700

    Init database dan auto migrate table

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/joho/godotenv"
+	"mri/client-email-sender/db"
 	"mri/client-email-sender/handler"
 	"mri/client-email-sender/handler/client"
+	"mri/client-email-sender/models"
 )
+
 func main() {
-	_= godotenv.Load()
+	_ = godotenv.Load()
+	db.InitConnectionFromEnvirontment().CreateNewConnection()
+	db.Connection.AutoMigrate(&models.LogEmail{})
 	server := handler.RunServer()
 
 	handlerClient := client.NewClientEmailHandler()
